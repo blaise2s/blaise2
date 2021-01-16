f2170af4c09b926065080a4836b8186010ff05c0
blaise2s <blaise.schaeffer@gmail.com>
2021-01-16 12:08:31 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'b2s-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'blaise2s';
+}
