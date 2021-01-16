1733de17d9f44653126eee7fc8649802322aea80
blaise2s <blaise.schaeffer@gmail.com>
2021-01-16 18:37:20 -0500

    feat(B2S-2): Starts header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+// src/app/header/header.component.ts
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'b2s-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
